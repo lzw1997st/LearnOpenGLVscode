4484d9f7c4d4a8828f3a994cc061749b36babc9f
Your Name <you@example.com>
2023-07-22 19:58:00 +0800

    02_hello_triangle VAO VBO EBO

--- src/02_hello_triangle/main.cpp
+++ src/02_hello_triangle/main.cpp
@@ -2,7 +2,7 @@
  * @Author: Your Name you@example.com
  * @Date: 2023-07-09 18:12:08
  * @LastEditors: Your Name you@example.com
- * @LastEditTime: 2023-07-22 18:32:34
+ * @LastEditTime: 2023-07-22 19:45:31
  * @FilePath: \LearnOpenGLVscode\src\01_hello_window\main.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -60,24 +60,34 @@ int main()
 
     // 定义顶点数组
     GLfloat vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f
+        0.5f, 0.5f, 0.0f,   // 右上角
+        0.5f, -0.5f, 0.0f,  // 右下角
+        -0.5f, -0.5f, 0.0f, // 左下角
+        -0.5f, 0.5f, 0.0f   // 左上角
     };
 
+    // 多个三角形很多重复节点，EBO 索引解决
+    GLuint indices[] = {
+        0, 1, 3, // 第一个三角形
+        1, 2, 3  // 第二个三角形
+    };
     // 创建顶点缓冲对象 VBO
     GLuint VBO;
     GLuint VAO;
+    GLuint EBO;
     glGenBuffers(1, &VBO);
     glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &EBO);
     // 绑定VAO对象
     glBindVertexArray(VAO);
 
     // 绑定缓冲
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     
     // 填充数据
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     
     // 解释顶点属性
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
@@ -143,8 +153,9 @@ int main()
 
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_POINTS, 0, 3);
-        glDrawArrays(GL_LINE_LOOP, 0, 3);
+        //glDrawArrays(GL_POINTS, 0, 6);
+        //glDrawArrays(GL_LINE_LOOP, 0, 6);
+        glDrawElements(GL_LINE_LOOP, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
 
         // 检查并调用事件，交换缓冲
@@ -167,3 +178,26 @@ void processInput(GLFWwindow* window)
         glfwSetWindowShouldClose(window, true);
     }
 }
+
+/* 最后的初始化和绘制代码现在看起来像这样：
+// ..:: 初始化代码 :: ..
+// 1. 绑定顶点数组对象
+glBindVertexArray(VAO);
+// 2. 把我们的顶点数组复制到一个顶点缓冲中，供OpenGL使用
+glBindBuffer(GL_ARRAY_BUFFER, VBO);
+glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+// 3. 复制我们的索引数组到一个索引缓冲中，供OpenGL使用
+glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+// 4. 设定顶点属性指针
+glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+glEnableVertexAttribArray(0);
+
+[...]
+
+// ..:: 绘制代码（渲染循环中） :: ..
+glUseProgram(shaderProgram);
+glBindVertexArray(VAO);
+glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+glBindVertexArray(0);
+*/
